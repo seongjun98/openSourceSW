666c3fabe14b90fc40209d528cab27c2bbca0ef2
leew0nseok <101381258+leew0nseok@users.noreply.github.com>
2022-03-18 17:35:11 +0900

    Update open.py

--- open.py
+++ open.py
@@ -3,8 +3,10 @@
 name = '이원석'
 SchoolNum = '201902702'
 Major = '컴퓨터전자시스템공학부'
+habbit = '운동'
 
 print("Hello World!\n")
 print("이름 =", name,
       "학번 =", SchoolNum,
-      "전공 =", Major)
+      "전공 =", Major,
+      "취미 =", )
