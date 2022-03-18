2a9561d62eb541ce5b059bff1aebd319456b2379
1eewonseok <90885230+1eewonseok@users.noreply.github.com>
2022-03-18 19:03:19 +0900

    Update open.py
    for pull request practice

--- open.py
+++ open.py
@@ -1,9 +1,12 @@
 이원석, 201902702, 컴퓨터전자시스템공학부
 
-name = '이원석'
-SchoolNum = '201902702'
-Major = '컴퓨터전자시스템공학부'
-habbit = '운동'
+print("본인의 이름, 학번, 전공, 취미 순으로 입력하세요(뛰어쓰기로 구분합니다)")
+name, SchoolNum, Major, habbit = map(str, input().split())
+
+# name = '이원석'
+# SchoolNum = '201902702'
+# Major = '컴퓨터전자시스템공학부'
+# habbit = '운동'
 
 print("Hello World!\n")
 print(f"이름 = {name}, 학번 = {SchoolNum}, 전공 = {Major}, 취미 = {habbit}")
