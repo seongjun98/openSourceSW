33e904db8419f9d445a36f43e60714e6c7e9fec0
leew0nseok <101381258+leew0nseok@users.noreply.github.com>
2022-03-18 17:42:42 +0900

    Update open.py

--- open.py
+++ open.py
@@ -6,7 +6,5 @@ Major = '컴퓨터전자시스템공학부'
 habbit = '운동'
 
 print("Hello World!\n")
-print("이름 =", name,
-      "학번 =", SchoolNum,
-      "전공 =", Major,
-      "취미 =", )
+print(f"이름 = {name}, 학번 = {SchoolNum}, 전공 = {Major}, 취미 = {habbit}")
+  
