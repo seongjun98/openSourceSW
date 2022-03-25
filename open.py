0f427f2bf0951f17cd0dd98699ddc8e766765d05
JJongss2 <101381470+JJongss2@users.noreply.github.com>
2022-03-25 14:53:28 +0900

    Update open.py
    좋아하는 노래 추가해주세요

--- open.py
+++ open.py
@@ -1,8 +1,8 @@
 Touched by lsy
 이원석, 201902702, 컴퓨터전자시스템공학부
 
-print("본인의 이름, 학번, 전공, 취미 순으로 입력하세요(뛰어쓰기로 구분합니다)")
-name, SchoolNum, Major, habbit = map(str, input().split())
+print("본인의 이름, 학번, 전공, 취미, 좋아하는 노래 순으로 입력하세요(뛰어쓰기로 구분합니다)")
+name, SchoolNum, Major, habbit, music = map(str, input().split())
 
 age = '23'
 
@@ -13,6 +13,7 @@ age = '23'
 
 print("Hello World!\n")
 print(f"이름 = {name}, 학번 = {SchoolNum}, 전공 = {Major}, 취미 = {habbit}")
+print("좋아하는 노래 = ", nusic)
 print("나이 = ", age)
 
 print("감사합니다")
